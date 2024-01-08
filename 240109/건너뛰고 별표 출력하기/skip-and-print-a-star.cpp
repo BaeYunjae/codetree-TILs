1ea327e08f3e6e80da7a9bf73fd42875e681f8c4
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-01-09 08:44:28 +0900

    Title: 건너뛰고 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240109/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
+++ 240109/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
@@ -10,14 +10,22 @@ int main() {
         for (int j = 0; j <= i; j++) {
             cout << "*";
         }
-        cout << endl << endl;
+        cout << "\n\n";
     }
 
-    for (int i = 1; i < n; i++) {
-        for (int j = n - 1; j >= i; j--) {
+    // for (int i = 1; i < n; i++) {
+    //     for (int j = n - 1; j >= i; j--) {
+    //         cout << "*";
+    //     }
+    //     cout << "\n\n";
+    // }
+
+    // 길이가 n - 1인 직각삼각형을 뒤집는다.
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j <= i; j++) {
             cout << "*";
         }
-        cout << endl << endl;
+        cout << "\n\n";
     }
 
 
