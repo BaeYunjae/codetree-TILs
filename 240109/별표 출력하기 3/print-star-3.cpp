9836f0b5063376b5eb9f64bd6dcfbfc7a6216d92
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-01-09 09:58:31 +0900

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240109/별표 출력하기 3/print-star-3.cpp
+++ 240109/별표 출력하기 3/print-star-3.cpp
@@ -7,14 +7,25 @@ int main() {
     cin >> n;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 2 * i ; j++) {
-            cout << " ";
+        for (int j = 0; j < i; j++) {
+            cout << "  ";               // 공백 2개
         }
-        for (int j = 2 * i; j < 2 * (n - 1) + 1; j++) {  // n = 5 / i = 0, 1, 2, 3, 4 / * : 9, 7, 5, 3, 1 
-            cout << "* "; 
+        for (int j = 0; j < (2 * n) - (2 * i) - 1; j++) {
+            cout << "* ";
         }
         cout << '\n';
     }
+
+
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < 2 * i ; j++) {
+    //         cout << " ";
+    //     }
+    //     for (int j = 2 * i; j < 2 * (n - 1) + 1; j++) {  // n = 5 / i = 0, 1, 2, 3, 4 / * : 9, 7, 5, 3, 1 
+    //         cout << "* "; 
+    //     }
+    //     cout << '\n';
+    // }
     
     return 0;
 }
