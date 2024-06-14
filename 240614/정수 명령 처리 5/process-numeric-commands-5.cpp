cdc8932475514f296fdb157ad89f248f151ec59b
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-06-14 22:36:10 +0900

    Title: 정수 명령 처리 5, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240614/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v;
+
+    string cmd;
+    int x;
+
+    while(n--){
+        cin >> cmd;
+
+        if (cmd == "push_back"){
+            cin >> x;
+            v.push_back(x);
+        }
+        else if (cmd == "pop_back"){
+            if (v.size() > 0) v.pop_back();
+        }
+        else if (cmd == "size"){
+            cout << v.size() << "\n";
+        }
+        else if (cmd == "get"){
+            if (v.size() >= k) cout << v[k - 1] << "\n";
+        }
+    }
+
+
+    return 0;
+}
