bfdf31676f57b515e3a332b8c4735f55bcc9edf7
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-07-02 21:59:32 +0900

    Title: 데이터 비교, Time: 118ms, Memory: 5MB, Status: Passed - CodeTree

--- 240702/데이터 비교/data-comparison.cpp
+++ 240702/데이터 비교/data-comparison.cpp
@@ -5,26 +5,28 @@ using namespace std;
 int main() {
     int n, m;
     int arr[100001];
+    int arr2[100001];
 
     cin >> n;
-
-    int num;
+    
     for (int i = 0; i < n; i++){
-        cin >> num;
-        arr[i] = num;
-    }
-
-    unordered_set<int> s;
-
-    for (int i = 0; i < n; i++){
-        s.insert(arr[i]);
+        cin >> arr[i];
     }
 
     cin >> m;
 
     for (int i = 0; i < m; i++){
-        cin >> num;
-        if (s.find(num) == s.end()){
+        cin >> arr2[i];
+    }
+
+
+    unordered_set<int> s;
+    for (int i = 0; i < n; i++){
+        s.insert(arr[i]);
+    }
+
+    for (int i = 0; i < m; i++){
+        if (s.find(arr2[i]) == s.end()){
             cout << 0 << " ";
         }
         else {
