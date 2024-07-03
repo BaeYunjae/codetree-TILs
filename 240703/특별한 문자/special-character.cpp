306dc8290976171e64df0936c30dfcf52016f78c
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-07-03 14:54:48 +0900

    Title: 특별한 문자, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 240703/특별한 문자/special-character.cpp
+++ 240703/특별한 문자/special-character.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
-#include <unordered_map>
 #include <string>
-#include <vector>
-#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
-bool compare(pair<char, pair<int, int>> a, pair<char, pair<int, int>> b){
-    if (a.second.second == b.second.second){
-        return a.second.first < b.second.first;
-    }
-    return a.second.second < b.second.second;
-}
+unordered_map<char, int> um;
+string str;
 
 int main() {
-    string str;
     cin >> str;
 
-    int idx = 0;
-    unordered_map<char, pair<int, int>> um;
     for (int i = 0; i < str.length(); i++){
-        if (um.find(str[i]) == um.end()){
-            um[str[i]].first = i;
+        um[str[i]]++;
+    }
+
+    for (int i = 0; i < str.length(); i++){
+        if (um[str[i]] == 1) {
+            cout << str[i];
+            return 0;
         }
-        um[str[i]].second++;
     }
-
-    vector<pair<char, pair<int,int>>> vect(um.begin(), um.end());
-    sort(vect.begin(), vect.end(), compare);
-
-    for (auto iter = vect.begin(); iter != vect.end(); iter++){
-        if (iter->second.second > 1) continue;
-        cout << iter -> first;
-        return 0;
-    }
-
 
     cout << "None";
+    
+    return 0;
 }
