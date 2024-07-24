c446c94935fe7b7672ccbd0d0758fcb353c20c44
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-07-24 23:59:14 +0900

    Title: 가까운 숫자, Time: 96ms, Memory: 4MB, Status: Passed - CodeTree

--- 240724/가까운 숫자/nearest-number.cpp
+++ 240724/가까운 숫자/nearest-number.cpp
@@ -1,43 +1,39 @@
 #include <iostream>
 #include <set>
-#include <climits>
 #include <algorithm>
-
-#define MAX_N 100000
-
 using namespace std;
 
-// 변수 선언
 int n;
-int queries[MAX_N];
+int queries[100001];
 set<int> s;
 
-int ans = INT_MAX;
+int ans = 21e8;
 
 int main() {
-    // 입력:
     cin >> n;
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++){
         cin >> queries[i];
+    }
 
-    // x = 0 위치에 점을 놓고 시작합니다.
+    // x = 0에 점을 놓고 시작
     s.insert(0);
 
-    for(int i = 0; i < n; i++) {
-        // 가장 근처에 있는 오른쪽 점을 찾습니다.
+    for (int i = 0; i < n; i++){
+        // 가장 근처에 있는 오른쪽 점 찾기
         set<int>::iterator it = s.upper_bound(queries[i]);
-        // 존재한다면, 거리 중 최솟값을 갱신합니다.
-        if(it != s.end())
+
+        // 존재한다면, 거리 중 최솟값 갱신
+        if (it != s.end()){
             ans = min(ans, *it - queries[i]);
-        
-        // 가장 근처에 있는 왼쪽 점을 찾습니다.
+        }
+
+        // 가장 근처에 있는 왼쪽 점 찾기
         it--;
-        // 거리 중 최솟값을 갱신합니다.
         ans = min(ans, queries[i] - *it);
 
-        // 해당 점을 treeset에 추가합니다.
         s.insert(queries[i]);
-        cout << ans << endl;
+        cout << ans << "\n";
     }
+
     return 0;
 }
