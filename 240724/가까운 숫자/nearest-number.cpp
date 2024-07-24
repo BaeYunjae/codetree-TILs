daa39877125d23a37fe5801bbd81055c11b7a2ed
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-07-24 23:47:06 +0900

    Title: 가까운 숫자, Time: 1097ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240724/가까운 숫자/nearest-number.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    set<int> s;
+    s.insert(0);
+    
+    while(n--){
+        int num;
+        cin >> num;
+        s.insert(num);
+
+        int minDist = 1e9;
+
+        int a, b;
+        set<int>::iterator it;
+        for (auto i = s.begin(); i != s.end(); i++){
+            if (*i == *s.rbegin()) break;
+            
+            it = s.upper_bound(*i);
+            a = *it;
+
+            it--;
+            b = *it;
+            
+            minDist = min(minDist, a - b);
+        }
+
+        cout << minDist << "\n";
+    }
+    
+
+
+    return 0;
+}
