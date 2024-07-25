7159786c53900d3fb7b39b7d517d63590147c219
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-07-25 21:56:51 +0900

    Title: 별표 출력하기 10, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240725/별표 출력하기 10/print-star-10.cpp
+++ 240725/별표 출력하기 10/print-star-10.cpp
@@ -5,58 +5,19 @@ int main() {
     int n;
     cin >> n;
 
-    int a = 1, b = n;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < 2 * n; i++){
         if (i % 2 == 0){
-            for (int j = 0; j < a; j++){
+            for (int j = 0; j < 1 + i / 2; j++){
                 cout << "* ";
             }
-            a++;
         }
         else{
-            for (int j = 0; j < b; j++){
+            for (int j = 0; j < n - (i - 1) / 2; j++){
                 cout << "* ";
             }
-            b--;
         }
         cout << "\n";
     }
-
-    if (n % 2 == 0){
-        for (int i = 0; i < n; i++){
-            if (i % 2 == 0){
-                for (int j = 0; j < a; j++){
-                    cout << "* ";
-                }
-                a++;
-            }
-            else{
-                for (int j = 0; j < b; j++){
-                    cout << "* ";
-                }
-                b--;
-            }
-            cout << "\n";
-        }
-    }
-    else{
-        for (int i = 0; i < n; i++){
-            if (i % 2 == 0){
-                for (int j = 0; j < b; j++){
-                    cout << "* ";
-                }
-                b--;
-            }
-            else{
-                for (int j = 0; j < a; j++){
-                    cout << "* ";
-                }
-                a++;
-            }
-            cout << "\n";
-        }
-    }
-
 
 
     return 0;
