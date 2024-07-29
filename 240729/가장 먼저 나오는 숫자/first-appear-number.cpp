1276612fe4962df7c5dd8bebb00721cc84a7d248
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-07-29 23:53:39 +0900

    Title: 가장 먼저 나오는 숫자, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240729/가장 먼저 나오는 숫자/first-appear-number.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[100001];
+
+int upperBound(int target){
+    int left = 0;
+    int right = n - 1;
+    int res = 1e9;
+
+    while (left <= right){
+        int mid = (left + right) / 2;
+
+        if (arr[mid] >= target){
+            right = mid - 1;
+            res = min(res, mid);
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+
+    if (arr[res] != target) return -1;
+
+    return res + 1;
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < m; i++){
+        int x;
+        cin >> x;
+        cout << upperBound(x) << "\n";
+    }
+    
+    return 0;
+}
