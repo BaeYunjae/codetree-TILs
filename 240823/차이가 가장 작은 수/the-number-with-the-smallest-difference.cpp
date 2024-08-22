09f5f7355c8710281246e49ec6a442dff5502dee
BaeYunjae <88019800+BaeYunjae@users.noreply.github.com>
2024-08-23 00:16:22 +0900

    Title: 차이가 가장 작은 수, Time: 120ms, Memory: 4MB, Status: Passed - CodeTree

--- 240823/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
+++ 240823/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
@@ -4,35 +4,43 @@
 using namespace std;
 
 int n, m;
+int nums[100001];
 set<int> s;
+
+int ans = 1e9 + 1;
 
 int main() {
     cin >> n >> m;
 
     for (int i = 0; i < n; i++){
-        int num;
-        cin >> num;
-
-        s.insert(num);
+        cin >> nums[i];
+        s.insert(nums[i]);
     }
 
-    int ans = 1e9 + 1;
-    for (auto now : s){
-        set<int>::iterator it = s.upper_bound(now);
-        while(1){
-            if (it == s.end() || *it - now > ans) break;
-            if (*it - (int)now >= m){
-                ans = min(ans, *it - (int)now);
-                break;
-            }
-            else{
-                it++;
-            }
+    for (int i = 0; i < n; i++){
+        int x = nums[i];
+        
+        // x보다 m 이상 더 크면서 가장 작은 값
+        // r - x >= m 만족하는 최소 r
+        // r >= m + x 만족하는 최소 r
+        if (s.lower_bound(m + x) != s.end()){
+            ans = min(ans, *s.lower_bound(m + x) - x);
         }
-    }
 
-    if (ans == 1e9 + 1) cout << -1;
-    else cout << ans;
+        // x보다 m 이상 더 작으면서 가장 큰 값
+        // x - r >= m
+        // r <= x - m
+        set<int>::iterator it = s.upper_bound(x - m);
+        if (it != s.begin()){
+            it--;
+            ans = min(ans, x - *it);
+        }
+
+    }
+    
+    if (ans == 1e9 + 1) ans = -1;
+    cout << ans;
+
 
     return 0;
 }
